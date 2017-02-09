b25258aa05c74d597eb13b001c31ce153b154fd7
Srivastava <587155@CTS.COM>
2017-02-09 18:26:59 +0530

    Tokenization

--- app/src/main/java/com/example/dpouch/MenuBean.java
+++ app/src/main/java/com/example/dpouch/MenuBean.java
@@ -16,9 +16,11 @@ public class MenuBean {
     }
      
      
-	public String getTitle() {
+	public String getTitle() 
+        {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
