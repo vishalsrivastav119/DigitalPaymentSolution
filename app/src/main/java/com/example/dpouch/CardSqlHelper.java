25be3f174a60fca0d722622d090ecdcd288f601b
Srivastava <587155@CTS.COM>
2017-02-10 15:24:32 +0530

    Tokenization

--- app/src/main/java/com/example/dpouch/CardSqlHelper.java
+++ app/src/main/java/com/example/dpouch/CardSqlHelper.java
@@ -44,7 +44,8 @@ public class CardSqlHelper extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
 
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
--- app/src/main/java/com/example/dpouch/TouchPay.java
+++ app/src/main/java/com/example/dpouch/TouchPay.java
@@ -19,6 +19,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
 import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
